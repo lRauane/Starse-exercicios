ea39822cf62a5956427e3a2aedfcb1e8f17bc382
Rauane <rauanelima7@gmail.com>
2022-06-24 16:23:16 -0300

    animações do jogo

--- Projeto mario/assets/JavaScript/script.js
+++ Projeto mario/assets/JavaScript/script.js
@@ -9,4 +9,21 @@ const jump = () => {
     }, 500)
 }
 
+const loopGame = setInterval(() => {
+    if (pipePosition <=  120 && pipePosition > 0 && marioPosition < 80)
+
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`
+
+    mario.src = "./assets/imagens/mario-game-over.png"
+    mario.styles.width = "75px";
+    mario.styles.marginLeft = "45px";
+}, 10)
+
+clearInterval(loopGame)
+
 document.addEventListener("keydown", jump)
+
